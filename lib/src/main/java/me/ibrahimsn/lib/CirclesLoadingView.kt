0f9ab1638bc7d193dc37bcf376145e3399a1cf5a
Ivanov <ivanovgo@m2.ru>
2024-03-13 18:58:44 +0300

    Add color attribute

--- lib/src/main/java/me/ibrahimsn/lib/CirclesLoadingView.kt
+++ lib/src/main/java/me/ibrahimsn/lib/CirclesLoadingView.kt
@@ -7,22 +7,18 @@ import android.animation.ValueAnimator.INFINITE
 import android.animation.ValueAnimator.REVERSE
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.*
 import me.ibrahimsn.lib.Constants.CIRCLE_COUNT
-import me.ibrahimsn.lib.Constants.COLOR_BLUE
-import me.ibrahimsn.lib.Constants.COLOR_GREEN
-import me.ibrahimsn.lib.Constants.COLOR_RED
-import me.ibrahimsn.lib.Constants.COLOR_YELLOW
 import me.ibrahimsn.lib.Constants.DEFAULT_ANIM_DELAY
 import me.ibrahimsn.lib.Constants.DEFAULT_ANIM_DISTANCE
 import me.ibrahimsn.lib.Constants.DEFAULT_ANIM_DURATION
 import me.ibrahimsn.lib.Constants.DEFAULT_ANIM_INTERPOLATOR
 import me.ibrahimsn.lib.Constants.DEFAULT_CIRCLE_MARGIN
 import me.ibrahimsn.lib.Constants.DEFAULT_CIRCLE_RADIUS
+import me.ibrahimsn.lib.Constants.DEFAULT_COLOR
 
 class CirclesLoadingView : View {
 
@@ -32,21 +28,22 @@ class CirclesLoadingView : View {
     private var animDuration = DEFAULT_ANIM_DURATION
     private var animDelay = DEFAULT_ANIM_DELAY
     private var animInterpolator = DEFAULT_ANIM_INTERPOLATOR
+    private var color = DEFAULT_COLOR
 
-    private var colors = listOf(COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_GREEN)
     private val positions = mutableListOf(0f, 0f, 0f, 0f)
     private val animatorSet = AnimatorSet()
 
     private val paint = Paint().apply {
         isAntiAlias = true
         style = Paint.Style.FILL
-        color = Color.RED
+        color = color
     }
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.CirclesLoadingView, 0, 0)
+        color = typedArray.getColor(R.styleable.CirclesLoadingView_circleColor, this.color)
         circleRadius = typedArray.getDimension(R.styleable.CirclesLoadingView_circleRadius, this.circleRadius)
         circleMargin = typedArray.getDimension(R.styleable.CirclesLoadingView_circleMargin, this.circleMargin)
         animDistance = typedArray.getDimension(R.styleable.CirclesLoadingView_animDistance, this.animDistance)
@@ -90,7 +87,6 @@ class CirclesLoadingView : View {
         var startPoint = width/2 - ((CIRCLE_COUNT-1) * (circleRadius + circleMargin/2))
 
         for (i in 0 until CIRCLE_COUNT) {
-            paint.color = colors[i]
             canvas.drawCircle(startPoint, height/2f + positions[i], circleRadius, paint)
             startPoint += (circleRadius*2) + circleMargin
         }
